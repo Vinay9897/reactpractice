c63bfdb1ba297d5d5402e2adc000afcc015ce314
Vinay Yadav <vinayyadav91190@gmail.com>
2022-06-20 22:16:01 +0530

    Create new file App.js to render all components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-import react from 'react'
-import reactdom from 'react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
+// function Hello() {
+//   return <p>{a + b}</p>;
+// }
+const a = 10;
+const b = 10;
+// ReactDOM.render(<Hello />, document.getElementById("root"));
 
+// class Operation extends React.Component {
+//   render() {
+//     fun = () => {
+//       <p>{a + b}</p>
+//     }
+//     return (
+//       <p></p>
+//     );
+//   }
+// };
+
+ReactDOM.render(<Operation />, document.getElementById("root"));
