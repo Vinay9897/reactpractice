4bc5302fac80526ef50dbbabeb64bd1c14407a74
Vinay Yadav <vinayyadav91190@gmail.com>
2022-11-11 17:02:56 +0530

    learn use the props with class component

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import './index.css';
 
 
 
-// function Component
+// function Component===================
 
 
-function Show() {
-    const name = "Vinay";
-    return <h1>hello {name}</h1>;
-}
+// function Show() {
+//     const name = "Vinay";
+//     return <h1>hello {name}</h1>;
+// }
 // ReactDOM.render(<Show />, document.getElementById('root'));
 
 
@@ -26,23 +26,36 @@ function Show() {
 // );
 
 
-//props
+//props================
 
-function Hello(props) {
-    return <p> Hi, {props.name}</p>
-}
+// function Hello(props) {
+//     return <p> Hi, {props.name}</p>
+// }
 // ReactDOM.render(<Hello name="Vinay" />, document.getElementById("root"));
 
 
-//Component returning components
+//Component returning components=====================
 
-function App() {
-    return <div>
-        <Hello name="vinay" />
-        <Show />
-    </div>
-}
+// function App() {
+//     return <div>
+//         <Hello name="vinay" />
+//         <Show />
+//     </div>
+// }
+// const el = <App />;
+// ReactDOM.render(el, document.getElementById("root"));
 
-const el = <App />;
-ReactDOM.render(el, document.getElementById("root"));
+
+// props with Class component======================
+
+// class Buy extends React.Component {
+//     render() {
+//         return <p> Hi , {this.props.name}</p>
+//     }
+// }
+// const el = <Buy name="Rahul" />;
+// ReactDOM.render(el, document.getElementById('root'));
+
+
+
 // export default Show;
