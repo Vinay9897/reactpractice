a1ec9e6cf481ba1bd559947343fc2c95f9d5e4a4
Vinay Yadav <vinayyadav91190@gmail.com>
2022-11-11 18:12:45 +0530

    learn to use setState() method

--- src/index.js
+++ src/index.js
@@ -57,5 +57,55 @@ import './index.css';
 // ReactDOM.render(el, document.getElementById('root'));
 
 
+// states===========================
+// need of state
+// props cannot be changed
+//In order to allow components to manage and change their data, 
+// React provides a feature called state.
+
+// State is object that is added as a property in class components.
+
+// class DemoClass extends React.Component {
+
+//     //state(object)
+//     //state is just a simple object , that
+//     // contains a key: value pairs
+//     state = {
+//         name: "vinay",
+//         age: 22,
+//     }
+//     render() {
+
+//         return <h1>Name : {this.state.name}.<br></br> Age: {this.state.age}</h1>;
+//     }
+// }
+
+// ReactDOM.render(<DemoClass />, document.getElementById("root"));
+
+// setState() method ===================================
+//state should not be modified directly.Instead,React provide the 
+// setState() method to modify the state.
+
+
+//Counter App with setState() method
+
+class Counter extends React.Component {
+    state = {
+        counter: 0
+    }
+    increment = () => {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    }
+
+    render() {
+        return <div className='container'>
+            {this.state.counter}
+            <button onClick={this.increment}>Increment</button>
+        </div>
+    }
+}
+ReactDOM.render(<Counter />, document.getElementById('root'));
 
 // export default Show;
